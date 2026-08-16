7b93b7d76daf16c136cd1a1125fb6741b788132b
jass-vimal <vimaljass8@gmail.com>
2026-08-16 12:32:15 +0530

    add contionals and string practice

--- js/phase1/phase1.js
+++ js/phase1/phase1.js
@@ -140,3 +140,143 @@
 // Use parseInt() on "500px".
 
 // console.log(parseInt("500px"))
+
+
+// Operators
+
+// Add two numbers and print the result.
+// 2. Find the remainder when 25 is divided by 4.
+// 3. Find the square of a number using exponent operator.
+// 4. Increment a variable using `++`.
+// 5. Decrement a variable using `-`.
+// 6. Use `+=` operator to increase a variable by 20.
+// 7. Compare two numbers using `>`, `<`, `>=`, `<=`.
+// 8. Check if two values are strictly equal using `===`.
+// 9. Compare `"10"` and `10` using both `==` and `===`.
+// 10. Create two boolean variables and test `&&`, `||`, and `!`.
+
+// console.log(10+20) 
+// console.log(25%4)
+// console.log(5 **2 )
+
+// let a = 5
+// a++
+// console.log(a)
+
+// let a = 6
+// a--
+// console.log(a)
+
+
+// let  a = 5;
+// a += 20
+// console.log(a)
+
+
+// Compare two numbers using `>`, `<`, `>=`, `<=`.
+
+// console.log(7 >5)
+// console.log(7<5)
+// console.log(7>=7)
+// console.log(7<=2)
+
+// Check if two values are strictly equal using `===`.
+
+
+// console.log("10" === 10)   //false it is equal in value but in the typeof not
+// console.log(10 === 10) //here value and type are equal
+
+
+// Compare `"10"` and `10` using both `==` and `===`.
+// console.log("10" == 10)  //value same
+// console.log("10" === 10)  //value same but type not same one is string anf second is number
+
+
+// 10. Create two boolean variables and test `&&`, `||`, and `!`.
+
+// let a = true;
+// let b = false;
+
+// console.log(a || b) //if one is true then true
+// console.log(a && b)  //if both are true then true otherwise one is false answer become false
+// console.log( !a)  //just opposite to answer 
+
+
+// ## Strings
+
+// 1. Create a string and print its length.
+// 2. Convert a string into uppercase.
+// 3. Convert a string into lowercase.
+// 4. Check if a string includes the word `"JavaScript"`.
+// 5. Extract the word `"World"` from `"Hello World"`.
+// 6. Replace `"apple"` with `"mango"` in a sentence.
+// 7. Split `"HTML,CSS,JS"` into an array.
+// 8. Remove extra spaces from a string.
+// 9. Repeat the word `"Hi"` 5 times.
+// 10. Print the first character of a string.
+// 11. Use template literals to print:`"My name is Aman and I am 20 years old"`
+
+
+// Create a string and print its length.
+// let name = "vimal"
+// console.log(name.length)  //5
+
+
+// Convert a string into uppercase.
+// let name = "vimmal"
+// console.log(name.toUpperCase())
+
+// Convert a string into lowercase.
+// console.log(name.toLowerCase())
+
+
+// Check if a string includes the word `"JavaScript"`.
+
+// let text = "i practice the question of javaScript daily"
+// console.log(text.includes("javaScript"))
+
+// Extract the word `"World"` from `"Hello World"`.
+
+// let string = "hello world"
+// console.log(string.slice(6))
+
+
+//Replace `"apple"` with `"mango"` in a sentence.
+
+// let taste = "i like to eat apple"
+// console.log(taste.replace("apple", "mango"))
+
+// Split `"HTML,CSS,JS"` into an array.
+// let split = "HTML CSS JS"
+// console.log(split.split(" "))  //[ 'HTML', 'CSS', 'JS' ]
+
+// Remove extra spaces from a string.
+// let name = "  hello  "
+// console.log(name)
+// console.log(name.trim())
+
+// Repeat the word `"Hi"` 5 times.
+// console.log("Hi ".repeat(5));
+
+// 10. Print the first character of a string.
+
+// let name = "cimmal"
+// console.log(name[0])
+// console.log(name.charAt(0))
+
+//  Use template literals to print:`"My name is Aman and I am 20 years old"`
+
+// console.log(`My name is Aman and I am 20 years old`)
+
+
+
+
+
+
+
+
+
+
+
+
+
